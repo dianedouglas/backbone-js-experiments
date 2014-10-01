74b9c2237ac9d084c8a318ca40b28383518db3e0
Diane Douglas <diane.douglas1@gmail.com>
2014-10-01 09:57:05 -0700

    Create a model with 2 instances and print out values.

---
+++ notes.js
@@ -0,0 +1,21 @@
+// models contain data for an app as well as logic around the data
+// ex concept of a todo item, including attributes like title/completed.
+// so it's a class.
+
+// create model
+var Todo = Backbone.Model.extend({});
+
+// instantiate
+var todo1 = new Todo();
+// with data. like hash in ruby. new Thing({ attribute1: 'blah', attribute2: 'bleeh'});
+var todo2 = new Todo({
+  title: 'Test',
+  completed: true
+});
+
+
+// for debugging, print it out.
+console.log(JSON.stringify(todo1));
+console.log(JSON.stringify(todo2));
+
+
