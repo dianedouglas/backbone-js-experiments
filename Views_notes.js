ef4a18182c5dae5de1dddc638c6242c255f4727b
dianedouglas <diane.douglas1@gmail.com>
2014-10-01 22:03:24 -0700

    View el property, setElement.

--- Views_notes.js
+++ Views_notes.js
@@ -42,10 +42,42 @@ var todoXView = new ToDoView({el: $('#footer')});
 // view.$(selector)
 // same as $(view.el).find(selector)
 
+// use setElement if you need to change to a different DOM element. 
+
+// We create two DOM elements representing buttons
+// which could easily be containers or something else
+var button1 = $('<button></button>');
+var button2 = $('<button></button>');
+
+var View = Backbone.View.extend({
+	events: {
+		click: function(e) {
+			console.log(view.el === e.target);
+			console.log('click event triggered.')
+		}
+	}
+});
+
+//make new instance of view. set on initialize to button1. triggering a click on button1 triggers click event in view.
+var view = new View({el: button1});
+button1.trigger('click');
+
+//reset view to use button2 as el. now triggering a click on button 2 triggers a click event in view. not in button 1 anymore.
+view.setElement(button2);
+button2.trigger('click');
+button1.trigger('click');
 
 
+// el property represents the markup portion of the view that will be rendered.
+// to get the view to actually render to the page, you need to add it as a new element or append it to an existing one.
 
+// We can also provide raw markup to setElement
+// as follows (just to demonstrate it can be done):
+// var view = new Backbone.View;
+// view.setElement('<p><a><b>test</b></a></p>');
+// console.log(view.$('a b').html()); // outputs "test"
 
+console.log('\n\n\n')
 
 
 
